ce66b2d31f0eb175b9147b7523861b21eab973ee
ayusshh66 <adonisjefrey66@gmail.com>
2026-04-07 00:24:30 +0530

    modules in nodejs

--- 01Node.js
+++ 01Node.js
@@ -1,1 +1,12 @@
-console.log("ji")
+const fs = require('fs')
+
+// console.log(fs)
+// this utf- makes the file in human readable form, else it will be in some number coded
+const content = fs.readFileSync('text.txt', 'utf-8')
+console.log(content);
+
+// writefilesync is to write , and appendfilesync is to append text in already made file
+
+fs.writeFileSync('sample1.txt', 'hey there', 'utf-8')
+fs.writeFileSync('sample2.txt',content,'utf-8')
+fs.appendFileSync('sample1.txt','yoooo','utf-8')
