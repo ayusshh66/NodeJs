2b7657edd4a3649028a54aa1a11f0d6b0705f8b0
ayusshh66 <adonisjefrey66@gmail.com>
2026-08-08 02:27:01 +0530

    made a queue for emails

---
+++ Redis/bullMQ/worker.ts
@@ -0,0 +1,28 @@
+import {Worker} from "bullmq"
+import {connection} from "./queue.ts"
+
+const worker = new Worker(
+    "emails",
+    async(job) =>{
+        console.log("processing email jobs", job.id, job.name, job.data)
+
+        await new Promise((resolve) => setTimeout(resolve,5000))
+
+        console.log("email job completed",job.id, job.name, job.data)
+    },
+    {
+        connection
+    }
+)
+
+worker.on("completed", (job) => {
+
+    console.log("job completed",job.id, job.name, job.data)
+
+})
+
+worker.on("failed", (job) => {
+
+    console.log("job failed", job?.id, job?.name, job?.data)
+
+})
